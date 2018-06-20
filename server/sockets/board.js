26dbf35ae95e8dc4360c0c42c26aaa9ffefa16f2
Arman-Zohrabyan <arman.zohrabyan.96@gmail.com>
2018-06-21 01:55:41 +0300

    added preview images

--- server/sockets/board.js
+++ server/sockets/board.js
@@ -8,20 +8,34 @@ const RoomsContainer = require('./classes/RoomsContainer.js');
 const container = new RoomsContainer();
 
 // start preview
-container.createRoom('sasdczxczxc123123njfas', 'Valodik');
 
-container.createRoom('sasdczxczxc123123njfddd', 'Addsdsdsd');
-container.userJoin('sasdczxczxc123123njfddd', 'sasdczxczxc123123njfqw');
-
-container.createRoom('sasdczxczxc123123nj', 'Valodik1');
+container.createRoom('sasdczxczxc123123nj', 'Player 1');
 container.userJoin('sasdczxczxc123123nj', 'sasdczxczxc123s123njfqw');
 container.startGame('sasdczxczxc123123nj');
 
-container.createRoom('sasdczxczxc123123njfac', 'Valodik2');
+container.createRoom('sasdczxczxc123123njfac', 'Player 2');
 container.userJoin('sasdczxczxc123123njfac', 'asd');
 container.startGame('sasdczxczxc123123njfac');
 container.userJoin('sasdczxczxc123123njfac', 'addsd');
 container.userJoin('sasdczxczxc123123njfac', 'www');
+
+container.createRoom('sasdczxczxc123123njfab', 'Player 3');
+container.userJoin('sasdczxczxc123123njfab', 'assd');
+container.startGame('sasdczxczxc123123njfab');
+container.userJoin('sasdczxczxc123123njfab', 'adwqxxdsd');
+container.userJoin('sasdczxczxc123123njfab', 'wwcxwxx');
+
+container.createRoom('sasdczxczxc123123njfas', 'Player 4');
+
+container.createRoom('sasdczxczxc123123njfassda', 'Player 5');
+
+container.createRoom('sasdczxczxc123123njfddd', 'Player 6');
+container.userJoin('sasdczxczxc123123njfddd', 'Andrey');
+
+container.createRoom('sasdczxczxc123123njfdddd', 'Player 7');
+container.userJoin('sasdczxczxc123123njfdddd', 'Andr');
+
+
 // end preview
 
 
