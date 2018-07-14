3c4cd535cd53b357798d3ceabf9a16151b6899f9
Arman-Zohrabyan <arman.zohrabyan.96@gmail.com>
2018-07-14 22:38:56 +0300

    responsive canvas

--- src/components/GamePage/GamePage.jsx
+++ src/components/GamePage/GamePage.jsx
@@ -22,7 +22,12 @@ class GamePage extends Component {
       myId: data.id,
       screen: {
         width: 0,
-        height: 0
+        height: 0,
+        coefficient: 0
+      },
+      mouseCoordinates: {
+        x: 0,
+        y: 0
       },
       context: null,
       currentGold: 100,
@@ -41,8 +46,34 @@ class GamePage extends Component {
         this.setState({ message });
       }
     });
+
+
     this.handleResize();
     window.addEventListener('resize',  this.handleResize.bind(this, false));
+
+
+    const context = this.refs.canvas.getContext('2d');
+    this.setState({ context: context });
+    requestAnimationFrame(() => {this.update()});
+  }
+
+  update = () => {
+    const { context, screen } = this.state;
+    // const keys = this.state.keys;
+
+    context.save();
+
+    // console.log(screen.coefficient);
+    context.beginPath();
+    context.arc( 1350*screen.coefficient, 800*screen.coefficient, 100*screen.coefficient, 0, 2 * Math.PI);
+    context.fill();
+    // context.stroke();
+
+
+    context.restore();
+
+    // Next frame
+    requestAnimationFrame(() => {this.update()});
   }
 
   componentWillUnmount() {
@@ -56,7 +87,8 @@ class GamePage extends Component {
     this.setState({
       screen : {
         width: document.body.clientWidth,
-        height: document.body.clientHeight
+        height: document.body.clientHeight,
+        coefficient: document.body.clientHeight/document.body.clientWidth
       }
     });
   }
@@ -66,14 +98,23 @@ class GamePage extends Component {
   }
 
   render() {
+    const {
+      screen,
+      mouseCoordinates
+    } = this.state;
+
     return (
-      <div>
+      <div className='canvas-container'>
         <div className='exitButton' onClick={this.handleBack} />    
         <canvas ref="canvas"
-          width={this.state.screen.width}
-          height={640}
-          style={{'width': '100%', 'height': this.state.screen.height}}
+          width={screen.width}
+          height={screen.height}
+          style={{'width': '100%', 'height': screen.height}}
         />
+        <div className='canvas-container_mouse-coordinates'>
+          <div>x: {mouseCoordinates.x}</div>
+          <div>y: {mouseCoordinates.y}</div>
+        </div>
       </div>
     );
   }
